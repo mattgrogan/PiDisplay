07b0de4b29d2962a0408e6aa81598443305ecdcc
Matthew Grogan <mvgnyc@gmail.com>
2026-02-11 16:59:15 -0500

    bug fixes

--- Main.gd
+++ Main.gd
@@ -4,9 +4,20 @@ func _ready():
 	# Set the window to fullscreen mode
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 
-# Handle input for closing the application
+# Handle input for closing the application and toggling fullscreen
 func _input(event):
 	if event is InputEventKey and event.pressed:
 		if event.keycode == KEY_ESCAPE:
 			print_debug("ESC pressed - closing application")
 			get_tree().quit()
+		elif event.keycode == KEY_F11:
+			toggle_fullscreen()
+
+func toggle_fullscreen():
+	var current_mode = DisplayServer.window_get_mode()
+	if current_mode == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		print_debug("F11 pressed - switching to windowed mode")
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	else:
+		print_debug("F11 pressed - switching to fullscreen mode")
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
--- Time.gd
+++ Time.gd
@@ -15,6 +15,8 @@ func _process(delta):
 		ampm = "PM"
 	if hour > 12:
 		hour = hour - 12
+	if hour == 0:
+		hour = 12
 	self.text = "%d:%02d" % [hour, time.minute]
 	$ampm.text = ampm
 	
