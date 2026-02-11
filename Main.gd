e8007a66239db85ee910d592697f882addcd7f5e
Matthew Grogan <mvgnyc@gmail.com>
2026-02-11 10:52:32 -0500

    fullscreen

--- Main.gd
+++ Main.gd
@@ -1,5 +1,9 @@
 extends Node
 
+func _ready():
+	# Set the window to fullscreen mode
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+
 # Handle input for closing the application
 func _input(event):
 	if event is InputEventKey and event.pressed:
